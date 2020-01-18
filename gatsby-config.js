52fc8aa2b23af8b99815535381df0c1dffb12432
ejarzo <ejarz25@gmail.com>
2020-01-18 12:46:58 -0500

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,13 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-69348531-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
