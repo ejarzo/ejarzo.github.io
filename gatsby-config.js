67923c5486e042e20186fc18d457387d80dbe167
Elias Jarzombek <ejarz25@gmail.com>
2020-01-28 15:21:55 -0500

    fix gatsby plugins nesting

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,13 +33,12 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    resolve: `gatsby-plugin-google-analytics`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-69348531-1",
-        head: false,
-        anonymize: true,
-        respectDNT: true,
+        trackingId: 'UA-69348531-1',
       },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
