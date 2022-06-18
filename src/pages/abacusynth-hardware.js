7ea77351d5a23594418d5cada0fe51d35391b6e3
Elias Jarzombek <ejarz25@gmail.com>
2022-06-18 00:34:14 -0400

    add links

--- src/pages/abacusynth-hardware.js
+++ src/pages/abacusynth-hardware.js
@@ -112,6 +112,37 @@ export default () => {
             </p>
 
             <p>
+              Featured on
+              <a href="https://hackaday.com/2022/06/15/abacus-synthesizer-really-adds-up/">
+                Hackaday
+              </a>
+              ,
+              <a href="https://musictech.com/news/meet-abacusyth-abacus-inspired-synthesiser/">
+                MusicTech
+              </a>
+              ,
+              <a href="https://blog.adafruit.com/2022/06/15/abacusynth-hardware-adafruit-johnedgarpark-adafruit/">
+                Adafruit
+              </a>
+              ,
+              <a href="https://makezine.com/2022/06/16/abacus-synth-abacasynth/">
+                Makezine
+              </a>
+              ,
+              <a href="https://www.hackster.io/news/elias-jarzombek-s-abacusynth-is-a-casual-creator-device-realized-in-wood-and-clever-electronics-9a7f4b9b7851">
+                Hackster.io
+              </a>
+              ,
+              <a href="https://www.gearnews.com/abacusynth-spinning-wheels-of-synthesis-in-a-digital-physical-hybrid/">
+                Gearnews
+              </a>
+              ,
+              <a href="https://www.keyboards.de/stories/interagieren-mit-dem-abacusynth/">
+                Keyboards DE
+              </a>
+              .
+            </p>
+            <p>
               Abacuysnth is a synthesizer inspired by an{' '}
               <a href="https://en.wikipedia.org/wiki/Abacus">abacus</a>, the
               ancient counting tool used all around the world. Just like an
