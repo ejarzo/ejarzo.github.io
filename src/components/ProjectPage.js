d22f674fadad68e78d6fc062224cda7ecd89644a
Elias Jarzombek <ejarz25@gmail.com>
2023-11-29 21:10:20 -0500

    mobile fix

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -46,93 +46,98 @@ const ProjectPage = props => {
           }}
         ></div>
         <div className="HeroImageWrapper">
-          <div style={{ height: 850, position: 'relative' }}>
-            <GatsbyImage
-              loading="eager"
-              image={bannerImage.childImageSharp.gatsbyImageData}
-              style={{
-                position: 'absolute',
-                height: '100%',
-                width: '100%',
-                zIndex: 1,
-                height: 850,
-                backgroundRepeat: 'none',
-                backgroundSize: `cover`,
-                backgroundPosition: `center`,
-              }}
-            />
-            <GatsbyImage
-              image={bannerImage.childImageSharp.gatsbyImageData}
-              style={{
-                position: 'absolute',
-                zIndex: 2,
-                height: '100%',
-                width: '100%',
-                backgroundRepeat: 'none',
-                backgroundSize: `cover`,
-                backgroundPosition: `center`,
-                filter: 'blur(20px)',
-                mask: `linear-gradient(180deg,
+          <GatsbyImage
+            loading="eager"
+            image={bannerImage.childImageSharp.gatsbyImageData}
+            style={{
+              position: 'absolute',
+              height: '100%',
+              width: '100%',
+              zIndex: 1,
+              backgroundRepeat: 'none',
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+            }}
+          />
+          <GatsbyImage
+            image={bannerImage.childImageSharp.gatsbyImageData}
+            style={{
+              position: 'absolute',
+              zIndex: 2,
+              height: '100%',
+              width: '100%',
+              backgroundRepeat: 'none',
+              backgroundSize: `cover`,
+              backgroundPosition: `center`,
+              filter: 'blur(20px)',
+              mask: `linear-gradient(180deg,
                   hsl(0, 0%, 0%) 0%,
                   hsl(0, 0%, 0%) 15%,
                   hsla(0, 0%, 0%, 0) 30%,
                   hsla(0, 0%, 0%, 0) 65%,
                   hsl(0, 0%, 0%) 100%
                 )`,
-              }}
-            />
-          </div>
+            }}
+          />
         </div>
         <div
-          className="ProjectTitleWrapper"
+          className="row"
           style={{
-            position: 'relative',
-            zIndex: 3,
-            margin: '0 auto',
             marginTop: -260,
-            // minHeight: heroHeight,
-            paddingTop: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            justifyContent: 'flex-start',
-            maxWidth: '720px',
-            // boxShadow: '0 20px 130px -20px rgba(247, 153, 33, 0.3)',
           }}
         >
-          <h1 style={{ margin: 0 }} className="ProjectPage__Title">
-            {title}
-          </h1>
-          {subtitle && (
-            <span
+          <div className="col-12" style={{ marginTop: 0, padding: 0 }}>
+            <div
+              className="ProjectTitleWrapper"
               style={{
-                fontSize: '0.8em',
-                textTransform: 'uppercase',
-                opacity: 1,
-                fontWeight: 'bold',
-                opacity: 0.8,
+                position: 'relative',
+                zIndex: 3,
+                margin: '0 auto',
+                // minHeight: heroHeight,
+                paddingTop: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                maxWidth: '720px',
+                // boxShadow: '0 20px 130px -20px rgba(247, 153, 33, 0.3)',
               }}
             >
-              {subtitle}
-            </span>
-          )}
-          {links && (
-            <div>
-              <div className="ProjectPage__Links">
-                {links.map(({ label, href, isPrimary }, i) => (
-                  <a
-                    className={`btn ${isPrimary ? 'btn--primary' : ''}`}
-                    key={label}
-                    href={href}
-                    target="blank"
-                    rel="noopener noreferrer"
-                  >
-                    {label}
-                  </a>
-                ))}
-              </div>
+              <h1 style={{ margin: 0 }} className="ProjectPage__Title">
+                {title}
+              </h1>
+              {subtitle && (
+                <span
+                  style={{
+                    fontSize: '0.8em',
+                    textTransform: 'uppercase',
+                    opacity: 1,
+                    fontWeight: 'bold',
+                    opacity: 0.8,
+                  }}
+                >
+                  {subtitle}
+                </span>
+              )}
+              {links && (
+                <div>
+                  <div className="ProjectPage__Links">
+                    {links.map(({ label, href, isPrimary }, i) => (
+                      <a
+                        className={`btn ${isPrimary ? 'btn--primary' : ''}`}
+                        key={label}
+                        href={href}
+                        target="blank"
+                        rel="noopener noreferrer"
+                      >
+                        {label}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
       </div>
       <div
